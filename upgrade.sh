8a5078ca13c5a7a3ff045490b2ab05912ba96b90
Bart Massey <bart@cs.pdx.edu>
2015-12-03 10:08:33 -0800

    added upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Copyright © 2015 Bart Massey
+# [This program is licensed under the GPL version 3 or later.]
+# Please see the file COPYING in the source
+# distribution of this software for license terms.
+
+# Upgrade the installed taunet-echo server.
+
+sudo rm /usr/local/sbin/taunet-echo
+sudo cp dist/build/taunet-echo/taunet-echo /usr/local/sbin/
+sudo service taunet-echo restart
